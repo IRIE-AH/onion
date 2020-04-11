1673640cc68cab0241db8bcfaedf5eb49cffed85
IRIE-AH <63456572+IRIE-AH@users.noreply.github.com>
2020-04-11 10:31:51 +0900

    Update 5min_bot.rb

--- 5min_bot.rb
+++ 5min_bot.rb
@@ -5,8 +5,8 @@ twClient = Twitter::REST::Client.new do |config|
     config.consumer_key    = ENV['MY_CONSUMER_KEY']
     config.consumer_secret = ENV['MY_CONSUMER_SECRET']
     config.access_token    = ENV['MY_ACCESS_TOKEN']
-    config.access_token_secret = EVN['MY_ACCESS_TOKEN_SECRET']
+    config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
 end
 
-#ツイートする文字列
+＃ツイートする文字列
 twClient.update("5分おきにツイートするテスト＾＾")
